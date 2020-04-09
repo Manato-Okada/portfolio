2f3476a121b630f10e0c17eb3f85b4d0c6bfd1cb
Manato-Okada <manato.okada@seattleconsulting.co.jp>
2020-04-09 17:30:03 +0900

    【ポートフォリオ実装】20200409

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,10 +1,34 @@
 <template>
-<div aboutTitle></div>
-<div aboutExplain></div>
+    <div id=aboutSection> 
+        <p class="aboutTitle">About Me</p>
+        <p class="aboutExplain">東京出身のWebエンジニア。</p>
+    </div>
 </template>
 
 <script>
 </script>
 
 <style scoped>
+  #aboutSection{
+    background-color: #f5f5f5;
+    width:100%
+  }
+  
+  .aboutTitle{
+    color: #20879f;
+    font-family: "Noto Sans";
+    font-weight: bold;
+    font-size: 18px;
+    text-shadow: 1px 1px #ffffff;
+  }
+
+  .aboutExplain{
+    color: #707070;
+    font-family: "Noto Sans";
+    font-size: 12px;
+    overflow-wrap: normal;
+  }
+
+
+
 </style>
